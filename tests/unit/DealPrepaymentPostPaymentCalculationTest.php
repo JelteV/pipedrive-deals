1327aa53df8a819e4d6ca82110d15794c1d20620
JelteV <foo@nope.com>
2024-06-12 21:26:36 +0200

    Fix name issue of dataProvider in DealPrepaymentPostPaymentCalculationTest

--- tests/unit/DealPrepaymentPostPaymentCalculationTest.php
+++ tests/unit/DealPrepaymentPostPaymentCalculationTest.php
@@ -18,9 +18,9 @@ class DealPrepaymentPostPaymentCalculationTest extends TestCase
     }
 
     /**
-     * @dataProvider testUnderPayCorrectionDataProvider
+     * @dataProvider underPaymentCorrectionDataProvider
      */
-    public function testUnderPayCorrection(Deal $deal): void
+    public function testUnderPaymentCorrection(Deal $deal): void
     {
         $prePayment = $deal->getField(EntityField::prePaymentField);
         $postPayment = $deal->getField(EntityField::postPaymentField);
@@ -61,7 +61,7 @@ class DealPrepaymentPostPaymentCalculationTest extends TestCase
         $this->assertEquals($deal->getValue(), $prePayment->getValue() + $postPayment->getValue());
     }
 
-    public static function testUnderPayCorrectionDataProvider(): array
+    public static function underPaymentCorrectionDataProvider(): array
     {
         return [
             [self::createDeal(10, 4, null)],
